3ffdff9b8990e5cb994dc2ce4342200b01549bcd
Ali Naqvi <naqv.ali.0@gmail.com>
2023-08-07 09:59:41 -0400

    environment configured, missing url dart define

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -19,4 +19,14 @@ class F {
     }
   }
 
+  static String get placeholderUrl {
+    switch (appFlavor) {
+      case Flavor.simpsonsviewer:
+        return 'https://cdn.vox-cdn.com/thumbor/lgOlyWAT77mWdutFCRS7k3m5KKI=/297x81:1426x928/1200x800/filters:focal(297x81:1426x928)/cdn.vox-cdn.com/uploads/chorus_image/image/35880068/simpsons.0.png';
+      case Flavor.wireviewer:
+        return 'https://static01.nyt.com/images/2008/03/10/arts/wiresec.jpg';
+      default:
+        return 'title';
+    }
+  }
 }
