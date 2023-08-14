cab84c8ea948acf7b293f294a35bc844caa90ee9
Ali Naqvi <naqv.ali.0@gmail.com>
2023-08-14 07:51:16 -0400

    chore cleanup

--- lib/character_shower/providers/character_shower_providers.dart
+++ lib/character_shower/providers/character_shower_providers.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: flutter_style_todos
+// // ignore_for_file: flutter_style_todos
 
-import 'package:dio/dio.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+// import 'package:dio/dio.dart';
+// import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'character_shower_providers.g.dart';
+// part 'character_shower_providers.g.dart';
 
-@riverpod
-Dio dio(DioRef ref) => Dio();
+// @riverpod
+// Dio dio(DioRef ref) => Dio();
