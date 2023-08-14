0a9c5c8782be47b3e5a08515e839452791e1f824
Ali Naqvi <naqv.ali.0@gmail.com>
2023-08-14 13:11:42 -0400

    refactor observer & logging cleanup

--- lib/main_simpsonsviewer.dart
+++ lib/main_simpsonsviewer.dart
@@ -3,8 +3,6 @@ import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:riverpod_base/app/app.dart';
-// import 'package:riverpod_base/app/app_provider_observer.dart';
 
 import 'package:show_characters_viewer/app/app.dart';
 
@@ -26,12 +24,3 @@ void main() {
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
-
-// import 'package:show_characters_viewer/flavors.dart';
-
-// import 'package:show_characters_viewer/main.dart' as runner;
-
-// Future<void> main() async {
-//   F.appFlavor = Flavor.simpsonsviewer;
-//   await runner.main();
-// }
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/anaqvi/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anaqvi/Desktop/remaster/show_character_viewer"
+export "FLUTTER_APPLICATION_PATH=/Users/anaqvi/Desktop/terminal/show_character_viewer"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
